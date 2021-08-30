16e125848198bd298e0f92a30938a312b212d9cb
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-30 23:56:22 +0300

    Create simple ui design

--- WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
+++ WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
@@ -2,10 +2,24 @@ package com.yagmurerdogan.weathermotionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.yagmurerdogan.weathermotionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startButton.setOnClickListener {
+            Log.e("Clicked", "Start")
+        }
+
     }
 }
