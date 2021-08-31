162d8dbd6828e7d0397d2c1b410e4b533fbbd6b1
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-31 11:34:11 +0300

    Create sequential and multiple anim functions

--- WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
+++ WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
@@ -4,7 +4,6 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.yagmurerdogan.weathermotionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +18,56 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.startButton.setOnClickListener {
-            startAnim(createAlphaAnimation())
-            startAnim(createTranslationAnimation())
-            startAnimSet(
+            createSequentialAnim()
+            //createMultipleAnim()
+            //startAnim(createAlphaAnimation())
+            //startAnim(createTranslationAnimation())
+            /*
+               startAnimSet(
                 createScaleXAnimation(),
                 createScaleYAnimation(),
+                repeatTransYAnim(),
                 createRotateAnimation()
             )
+             */
         }
+    }
+    private fun createSequentialAnim() {
+        val alphaAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"alpha",1.0f,0.0f)
+        val scaleXAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"scaleX",1.0f,2.0f)
+        val scaleYAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"scaleY",1.0f,2.0f)
+
+        val multiple = AnimatorSet().apply {
+            duration = 1000
+            playSequentially(scaleXAnim,scaleYAnim,alphaAnim)
+        }
+        multiple.start()
+    }
+
+    private fun createMultipleAnim() {
+        val alphaAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"alpha",1.0f,0.0f)
+        val scaleXAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"scaleX",1.0f,2.0f)
+        val scaleYAnim = ObjectAnimator.ofFloat(binding.cloudImageView,"scaleY",1.0f,2.0f)
+
+        val multiple = AnimatorSet().apply {
+            duration = 1000
+            playTogether(alphaAnim,scaleXAnim,scaleYAnim)
+        }
+        multiple.start()
+    }
+
+    private fun repeatTransYAnim(): ObjectAnimator {
+        val repeat = ObjectAnimator.ofFloat(
+            binding.cloudImageView,
+            "translationY",
+            0.0f,
+            100.0f
+        ).apply {
+            duration = 1000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+        return repeat
     }
 
     private fun createAlphaAnimation(): ObjectAnimator {
@@ -73,6 +114,8 @@ class MainActivity : AppCompatActivity() {
             2.0f
         ).apply {
             duration = 2000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
         }
         return scaleYAnim
     }
@@ -85,6 +128,8 @@ class MainActivity : AppCompatActivity() {
             2.0f
         ).apply {
             duration = 2000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
         }
         return scaleXAnim
     }
