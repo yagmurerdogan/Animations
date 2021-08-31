d9690ded3bc005a4a4caf5a362c432b727561f6f
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-31 09:46:35 +0300

    Create rotate animation

--- WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
+++ WeatherMotionLayout/app/src/main/java/com/yagmurerdogan/weathermotionlayout/MainActivity.kt
@@ -5,7 +5,6 @@ import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import com.yagmurerdogan.weathermotionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,15 +19,40 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.startButton.setOnClickListener {
-            Log.e("Clicked", "Start")
-            //startAlphaAnimation()
-            startAnimSet(startScaleXAnimation(), startScaleYAnimation())
+            //startAnim(createAlphaAnimation())
+            startAnimSet(
+                createScaleXAnimation(),
+                createScaleYAnimation(),
+                createRotateAnimation()
+            )
 
         }
-
+    }
+    private fun createAlphaAnimation(): ObjectAnimator {
+        val alphaAnim = ObjectAnimator.ofFloat(
+            binding.weatherTextView,
+            "alpha",
+            1.0f,
+            0.0f
+        ).apply {
+            duration = 4000
+        }
+        return alphaAnim
     }
 
-    private fun startScaleYAnimation(): ObjectAnimator {
+    private fun createRotateAnimation() : ObjectAnimator {
+        val rotateAnim = ObjectAnimator.ofFloat(
+            binding.cloudImageView,
+            "rotation",
+            0.0f,
+            360.0f
+        ).apply {
+            duration = 2000
+        }
+        return rotateAnim
+    }
+
+    private fun createScaleYAnimation(): ObjectAnimator {
         val scaleYAnim = ObjectAnimator.ofFloat(
             binding.cloudImageView,
             "scaleY",
@@ -40,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         return scaleYAnim
     }
 
-    private fun startScaleXAnimation(): ObjectAnimator {
+    private fun createScaleXAnimation(): ObjectAnimator {
         val scaleXAnim = ObjectAnimator.ofFloat(
             binding.cloudImageView,
             "scaleX",
@@ -52,22 +76,15 @@ class MainActivity : AppCompatActivity() {
         return scaleXAnim
     }
 
-
-    private fun startAnimSet(anim1: ObjectAnimator, anim2: ObjectAnimator) {
-        val animationSet = AnimatorSet()
-        animationSet.playTogether(anim1, anim2)
-        animationSet.start()
+    private fun startAnim(anim: ObjectAnimator) {
+        anim.start()
     }
 
-    private fun startAlphaAnimation() {
-        val alphaAnim = ObjectAnimator.ofFloat(
-            binding.weatherTextView,
-            "alpha",
-            1.0f,
-            0.0f
-        ).apply {
-            duration = 2000
+    private fun startAnimSet(vararg anims: ObjectAnimator) {
+        val animationSet = AnimatorSet()
+        for (anim in anims) {
+            animationSet.playTogether(anim)
         }
-        alphaAnim.start()
+        animationSet.start()
     }
 }
