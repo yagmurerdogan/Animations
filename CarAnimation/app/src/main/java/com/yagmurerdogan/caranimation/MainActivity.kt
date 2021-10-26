d0a40453ae549c84faafb847243c4b4b2d563fa3
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-10-26 13:11:28 +0300

    Refactor code blocks

--- CarAnimation/app/src/main/java/com/yagmurerdogan/caranimation/MainActivity.kt
+++ CarAnimation/app/src/main/java/com/yagmurerdogan/caranimation/MainActivity.kt
@@ -14,7 +14,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            MainAnimation.animateMainActivity(binding)
+            animateMainActivity()
         }
+    }
+
+    private fun animateMainActivity() {
+        startAnimSet(
+            with(binding) {
+                arrayOf(cloudImageView, cloudImageView2, cloudImageView3).forEach { cloudImage ->
+                    cloudImage.createAnim(TRANSLATION_X, ZERO_FLOAT, NEGATIVE_HALF_TOUR)
+                        .makeInfiniteAndReverse()
+                }
+                button.createAnim(ALPHA, ONE_FLOAT, ZERO_FLOAT, ONE_SECOND)
+                textView.createAnim(ALPHA, ONE_FLOAT, ZERO_FLOAT, ONE_SECOND)
+                carImageView.createAnim(TRANSLATION_Y, ONE_FLOAT, CAR_ROAD)
+                carImageView.createAnim(SCALE_X, ONE_FLOAT, HALF_FLOAT).makeInfiniteAndReverse()
+                carImageView.createAnim(SCALE_Y, ONE_FLOAT, HALF_FLOAT).makeInfiniteAndReverse()
+                sunImageView.createAnim(ROTATION, ZERO_FLOAT, FULL_TOUR).makeInfiniteAndReverse()
+                nightLayout.createAnim(ALPHA, ZERO_FLOAT, ONE_FLOAT).makeInfiniteAndReverse()
+                sunImageView.createAnim(ALPHA, ONE_FLOAT, ZERO_FLOAT).makeInfiniteAndReverse()
+                moonImageView.createAnim(ALPHA, ZERO_FLOAT, ONE_FLOAT).makeInfiniteAndReverse()
+            }
+        )
     }
 }
--- CarAnimation/app/src/main/java/com/yagmurerdogan/caranimation/MainAnimation.kt
+++ CarAnimation/app/src/main/java/com/yagmurerdogan/caranimation/MainAnimation.kt
@@ -2,132 +2,60 @@ package com.yagmurerdogan.caranimation
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.media.Image
 import android.util.Property
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.yagmurerdogan.caranimation.databinding.ActivityMainBinding
 
 /**
  * Created by Yağmur ERDOĞAN on 23 October 2021
  */
 
-object MainAnimation {
-    fun animateMainActivity(binding: ActivityMainBinding) {
-        startAnimSet(
-            createAnim(binding.button, ALPHA, ONE_FLOAT, ZERO_FLOAT, ONE_SECOND),
-            createAnim(binding.textView, ALPHA, ONE_FLOAT, ZERO_FLOAT, ONE_SECOND),
-            createAnim(binding.carImageView, TRANSLATION_Y, ONE_FLOAT, CAR_ROAD, FOUR_SECONDS),
-            createAnim(
-                binding.carImageView,
-                SCALE_X,
-                ONE_FLOAT,
-                HALF_FLOAT,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.carImageView,
-                SCALE_Y,
-                ONE_FLOAT,
-                HALF_FLOAT,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.sunImageView,
-                ROTATION,
-                ZERO_FLOAT,
-                FULL_TOUR,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.nightLayout,
-                ALPHA,
-                ZERO_FLOAT,
-                ONE_FLOAT,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.sunImageView,
-                ALPHA,
-                ONE_FLOAT,
-                ZERO_FLOAT,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.moonImageView,
-                ALPHA,
-                ZERO_FLOAT,
-                ONE_FLOAT,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.cloudImageView,
-                TRANSLATION_X,
-                ZERO_FLOAT,
-                NEGATIVE_HALF_TOUR,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.cloudImageView2,
-                TRANSLATION_X,
-                ZERO_FLOAT,
-                NEGATIVE_HALF_TOUR,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-            createAnim(
-                binding.cloudImageView3,
-                TRANSLATION_X,
-                ZERO_FLOAT,
-                NEGATIVE_HALF_TOUR,
-                FOUR_SECONDS
-            ).makeInfiniteAndReverse(),
-        )
+ val ROTATION: Property<View, Float> = View.ROTATION
+ val ALPHA: Property<View, Float> = View.ALPHA
+ val TRANSLATION_Y: Property<View, Float> = View.TRANSLATION_Y
+ val TRANSLATION_X: Property<View, Float> = View.TRANSLATION_X
+ val SCALE_X: Property<View, Float> = View.SCALE_X
+ val SCALE_Y: Property<View, Float> = View.SCALE_Y
+ const val FOUR_SECONDS = 4000L
+ const val ONE_SECOND = 1000L
+ const val ONE_FLOAT = 1.0f
+ const val HALF_FLOAT = 0.5f
+ const val ZERO_FLOAT = 0.0f
+ const val FULL_TOUR = 360.0f
+ const val NEGATIVE_HALF_TOUR = -180.0f
+ const val CAR_ROAD = -130.0f
+
+ fun View.createAnim(
+    property: Property<View, Float>,
+    startValue: Float,
+    endValue: Float,
+    time: Long = FOUR_SECONDS
+): ObjectAnimator {
+    return ObjectAnimator.ofFloat(
+        this,
+        property,
+        startValue,
+        endValue
+    ).apply {
+        duration = time
     }
+}
 
-    private fun createAnim(
-        view: View,
-        property: Property<View, Float>,
-        startValue: Float,
-        endValue: Float,
-        time: Long
-    ): ObjectAnimator {
-        return ObjectAnimator.ofFloat(
-            view,
-            property,
-            startValue,
-            endValue
-        ).apply {
-            duration = time
-        }
+ fun ObjectAnimator.makeInfiniteAndReverse(mode: Int = ObjectAnimator.REVERSE): ObjectAnimator {
+    return this.apply {
+        repeatCount = ObjectAnimator.INFINITE
+        repeatMode = mode
     }
+}
 
-    private fun ObjectAnimator.makeInfiniteAndReverse(): ObjectAnimator {
-        return this.apply {
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }
+ fun startAnimSet(vararg animations: ObjectAnimator) {
+    val animationSet = AnimatorSet()
+    for (animation in animations) {
+        animationSet.playTogether(animation)
     }
-
-    private fun startAnimSet(vararg animations: ObjectAnimator) {
-        val animationSet = AnimatorSet()
-        for (animation in animations) {
-            animationSet.playTogether(animation)
-        }
-        animationSet.start()
-    }
-
-
-        private val ROTATION: Property<View, Float> = View.ROTATION
-        private val ALPHA: Property<View, Float> = View.ALPHA
-        private val TRANSLATION_Y: Property<View, Float> = View.TRANSLATION_Y
-        private val TRANSLATION_X: Property<View, Float> = View.TRANSLATION_X
-        private val SCALE_X: Property<View, Float> = View.SCALE_X
-        private val SCALE_Y: Property<View, Float> = View.SCALE_Y
-        private const val FOUR_SECONDS = 4000L
-        private const val ONE_SECOND = 1000L
-        private const val ONE_FLOAT = 1.0f
-        private const val HALF_FLOAT = 0.5f
-        private const val ZERO_FLOAT = 0.0f
-        private const val FULL_TOUR = 360.0f
-        private const val NEGATIVE_HALF_TOUR = -180.0f
-        private const val CAR_ROAD = -130.0f
-
+    animationSet.start()
 }
